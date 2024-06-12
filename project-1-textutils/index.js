44f2108747dd1c53c3a86307718169f4bf224d3f
codewithlaksh <dashlakshyaraj2006@gmail.com>
2024-06-12 19:25:32 +0530

    Project 1: TextUtils - Add form actions to analyze text - Video #7

--- project-1-textutils/index.js
+++ project-1-textutils/index.js
@@ -8,14 +8,37 @@ const PORT = 8080 || process.env.PORT;
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 app.get('/', (req, res) => {
     // res.status(200).send('Textutils Project');
     res.status(200).render('index')
 })
 
-app.get('/analyzed', (req, res) => {
+app.post('/analyzed', (req, res) => {
     // res.status(200).send('Textutils Project');
-    res.status(200).render('analyzed')
+    const text = req.body.textInput;
+    const purpose = req.body.analyzeAction;
+    let analyzedText = "";
+
+    if (purpose === "toUpperCase") {
+        analyzedText = text.toUpperCase();
+    }
+    if (purpose === "toLowerCase") {
+        analyzedText = text.toLowerCase();
+    }
+    if (purpose === "removeExtraSpaces") {
+        analyzedText = text.replace(/\s+/g, " ");
+    }
+    if (purpose === "removePunctuation") {
+        analyzedText = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    }
+    if (purpose === "removeNewLines") {
+        analyzedText = text.replace(/\n+/g, " ");
+    }
+
+    res.status(200).render('analyzed', {analyzedText: analyzedText, purpose:purpose})
 })
 
 app.listen(PORT, () => {
