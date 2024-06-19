687b4a0682523a43b1278bbc2e9b5053a5c62aa4
codewithlaksh <dashlakshyaraj2006@gmail.com>
2024-06-19 19:39:52 +0530

    Project 2: CRUD App - Add alert message flashing & fix jquery error - Video #15

--- project-2-crud/app.js
+++ project-2-crud/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectToMongo = require('./lib/db');
 const path = require('path');
 const ejs = require('ejs');
+const session = require('express-session');
 const app = express();
 
 const PORT = 8080 || process.env.PORT;
@@ -11,12 +12,29 @@ app.use(express.urlencoded({
     extended: false
 }))
 
+app.use(session({
+    secret: 'keyboard cat', // store in .env file
+    resave: false,
+    saveUninitialized: false,
+}))
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 connectToMongo()
+
+// Middleware to flash messages
+app.use((req, res, next) => {
+    if (req.session.message) {
+        res.locals.message = req.session.message;
+        delete req.session.message;
+    } else {
+        res.locals.message = null;
+    }
+    next();
+})
 
 app.use('/', require('./routes/index'))
 app.use('/api', require('./routes/api'))
--- project-2-crud/routes/api.js
+++ project-2-crud/routes/api.js
@@ -3,22 +3,55 @@ const Notes = require('../models/Notes');
 const router = express.Router();
 
 router.post('/add', async (req, res) => {
-    const note = new Notes(req.body);
-    await note.save();
-    
-    return res.redirect('/')
+    if (req.body.noteDesc === "") {
+        req.session.message = {
+            status: 'error',
+            msg: 'Please fill up all the fields!'
+        }
+        
+        return res.redirect('/')
+    } else {
+        const note = new Notes(req.body);
+        await note.save();
+        
+        req.session.message = {
+            status: 'success',
+            msg: 'Your note has been added successfully!'
+        }
+
+        return res.redirect('/')
+    }
 })
 
 router.post('/update', async (req, res) => {
-    await Notes.findOneAndUpdate({uuid: req.body.uuid}, {
-        $set: {noteDesc: req.body.noteDesc}
-    })
+    if (req.body.noteDesc === "") {
+        req.session.message = {
+            status: 'eror',
+            msg: 'Please fill up all the fields!'
+        }
 
-    return res.redirect('/edit/'+req.body.uuid)
+        return res.redirect('/edit/' + req.body.uuid)
+    } else {
+        await Notes.findOneAndUpdate({ uuid: req.body.uuid }, {
+            $set: { noteDesc: req.body.noteDesc }
+        })
+
+        req.session.message = {
+            status: 'success',
+            msg: 'Your note has been updated successfully!'
+        }
+
+        return res.redirect('/edit/' + req.body.uuid)
+    }
 })
 
-router.post('/delete', async(req, res) => {
-    await Notes.findOneAndDelete({uuid: req.body.uuid});
+router.post('/delete', async (req, res) => {
+    await Notes.findOneAndDelete({ uuid: req.body.uuid });
+
+    req.session.message = {
+        status: 'success',
+        msg: 'Your note has been deleted successfully!'
+    }
 
     return res.redirect('/')
 })
